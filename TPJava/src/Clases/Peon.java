f73cb7c3d914fe02d8d2c14faa47be51cc2c6ae7
David Martinez <david@dave.mshome.net>
2015-10-08 05:10:18 -0300

    Peon arreglado + validacion de sus movimientos

--- TPJava/src/Clases/Peon.java
+++ TPJava/src/Clases/Peon.java
@@ -1,5 +1,7 @@
 package Clases;
 
+import CapadeNegocio.ControladorJuego;
+
 public class Peon extends Pieza {
 	
 	
@@ -11,18 +13,19 @@ public class Peon extends Pieza {
 	public  boolean validarMovimiento(String a, String b,String color) {
 		int c=Integer.parseInt(a.substring(1));  //analizo la parte numerica de la posicion(cadena)
 		int d=Integer.parseInt(b.substring(1));  //analizo la parte numerica de la posicion(cadena)
-	
-	int distancia = color.contentEquals("negra")? (c-d) : (d-c);  //Hago la pregunta de que color es
-	if(distancia==1)                                               //el calculo de la distancia dependera de que color sea,esto se debe
+	    ControladorJuego cp = new ControladorJuego();
+	int distancia = color.contentEquals("Blanco")? (c-d) : (d-c);  //Hago la pregunta de que color es
+	if(distancia==1 && !a.substring(0,1).equals(b.substring(0,1)) && c!=d && cp.buscarPieza(b)!=null )                                               //el calculo de la distancia dependera de que color sea,esto se debe
 		                                                             // a como avanzan las fichas blancas y las negras entonces sera 
-		                                                             // un movimiento valido si la distancia entre que recorre es 1 
-		                                                             // si la distancia es negativa recorreria hacia atras y no es valido
+		                                                             // un movimiento valido si la distancia entre que recorre es 1	                                                             // si la distancia es negativa recorreria hacia atras y no es valido
 	{
 		return true;
 	}
 	else
 	{
-	return false;
+      if(distancia==1 && a.substring(0,1).equals(b.substring(0,1)) && c!=d)
+    	  return true;
+      else return false;
 	}
 	}
 	
